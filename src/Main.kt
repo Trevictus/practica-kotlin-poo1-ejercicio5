208124069fbf0711096d4f2c1b8888ff42f3ab6f
vgomtej353 <gtvictor@iesrafaelalberti.es>
2025-01-20 21:36:08 +0100

    commit 2 ejercicio5 poo

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+fun main(){
+
+
+    val timepo1 = Tiempo(0,62,123)
+
+
+}
--- src/Tiempo.kt
+++ src/Tiempo.kt
@@ -1,2 +1,21 @@
-class Tiempo {
+class Tiempo(var horas: Int, var minutos: Int, var segundos: Int) {
+
+
+
+    init{
+
+
+
+        require(horas in 0..24){"Las hora debe estar entre 0 y 24."}
+        require(minutos > 0){"Los minutos deben ser un nº positivo entero."}
+        require(segundos > 0){"Los segundos deben ser un nº positivo entero."}
+    }
+
+    constructor(horas: Int): this(horas, minutos = 0, segundos = 0)
+
+    constructor(horas: Int, minutos: Int): this(horas, minutos, segundos = 0)
+
+    override fun toString(): String {
+        return "${horas}h:${minutos}m:${segundos}s"
+    }
 }
